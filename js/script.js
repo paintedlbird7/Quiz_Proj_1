e0b91dba96f10ba50d0f926deb007d19c0c85962
Rosa M Perez <rosaperezlarios@gmail.com>
2025-02-26 13:23:59 -0800

    organized files

--- js/script.js
+++ js/script.js
@@ -1,38 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  /*-------------- Constants -------------*/
-  const questions = {
-    frida: [
-      {
-        question:
-          "Frida Kahlo exhibited her paintings in Mexico and Paris before her death in 1954?",
-        options: ["True", "False"],
-        answer: "True",
-      },
-      {
-        question: "Frida Kahlo was married to whom?",
-        options: ["Diego Rivera", "Leon Trotsky", "Tina Modotti"],
-        answer: "Diego Rivera",
-      },
-      {
-        question:
-          "Frida Kahlo painted a painting with this animal on her shoulder?",
-        options: ["bird", "monkey", "gorilla"],
-        answer: "monkey",
-      },
-    ],
-    general: [
-      {
-        question: "Is Paris the capital of France?",
-        options: ["True", "False"],
-        answer: "True",
-      },
-      {
-        question: "Is the sun a star?",
-        options: ["True", "False"],
-        answer: "True",
-      },
-    ],
-  };
+  
 
   /*---------- Variables (state) ---------*/
   let currentCategory = "";
